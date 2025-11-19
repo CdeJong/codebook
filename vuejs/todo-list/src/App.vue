77825d4d48297cb823734ef5a180b4ec462980ce
CdeJong <christiaan240@gmail.com>
2025-11-19 12:39:15 +0100

    Added Vue.js assignment 5

---
+++ vuejs/todo-list/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {ref} from 'vue';
+
+const tasks = ref([
+  { name: 'Boodschappen doen', completed: false},
+  { name: 'Afwassen', completed: true},
+  { name: 'Hond uitlaten', completed: false},
+]);
+
+const toggleTask = (task) => {
+  task.completed = !task.completed;
+}
+
+</script>
+
+<template>
+    <h1>Todo List</h1>
+    <ul>
+        <li v-for="(task, index) in tasks" :key="index">{{ task.name }} {{ task.completed ? "(voltooid)" : "(niet voltooid)" }} <button @click="toggleTask(task)">Toggle</button></li>
+    </ul>
+</template>
+
+<style scoped></style>
