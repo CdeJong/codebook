1c3a9baf9c5658c38359c5eac8785f05b1c640b7
CdeJong <christiaan240@gmail.com>
2025-11-19 11:45:29 +0100

    Added Vue.js assignments 1-4

---
+++ vuejs/Vue-Project-Starten/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {reactive, ref} from 'vue';
+
+const clicks = ref(0);
+const person = reactive({
+  firstName: '',
+  country: ''
+});
+
+const increaseClicks = () => {
+  clicks.value++;
+}
+
+const changeCountry = (newCountry) => {
+  person.country = newCountry;
+}
+
+</script>
+
+<template>
+    <button @click="increaseClicks">Click me!</button>
+    <button @click="changeCountry('Nederland')">Nederland</button>
+    <button @click="changeCountry('België')">België</button>
+    <input type="text" v-model="person.firstName" placeholder="First name" />
+    <p>Clicks: {{ clicks }}</p>
+    <p>First Name: {{ person.firstName }}</p>
+    <p>Country: {{ person.country }}</p>
+</template>
+
+<style scoped></style>
