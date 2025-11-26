4584d87b9b73a4beca1d4fcb9666125b5bcf0f90
CdeJong <christiaan240@gmail.com>
2025-11-26 10:47:37 +0100

    Removed unneeded default checks

--- vuejs/vue-yahtzee/src/components/ScoreTable.vue
+++ vuejs/vue-yahtzee/src/components/ScoreTable.vue
@@ -26,14 +26,14 @@ const countResult = computed(() => {
 });
 
 const scoreCount = (value, countResult) => {
-	const count = countResult[value];
+    const count = countResult[value];
 
-	if (count == null) {
-		return 0;
-	}
+    if (count == null) {
+        return 0;
+    }
 
-	return count * value;
-}
+    return count * value;
+};
 
 const scoreXOfAKind = (amount, countResult, score) => {
     let result = 0;
@@ -73,16 +73,16 @@ const hasStraight = (length, countResult) => {
     return highestLength >= length;
 };
 
-const aces = computed(() => scoreCount(1, countResult.value) ?? 0);
-const twos = computed(() => scoreCount(2, countResult.value) ?? 0);
-const threes = computed(() => scoreCount(3, countResult.value) ?? 0);
-const fours = computed(() => scoreCount(4, countResult.value) ?? 0);
-const fives = computed(() => scoreCount(5, countResult.value) ?? 0);
-const sixes = computed(() => scoreCount(6, countResult.value) ?? 0);
+const aces = computed(() => scoreCount(1, countResult.value));
+const twos = computed(() => scoreCount(2, countResult.value));
+const threes = computed(() => scoreCount(3, countResult.value));
+const fours = computed(() => scoreCount(4, countResult.value));
+const fives = computed(() => scoreCount(5, countResult.value));
+const sixes = computed(() => scoreCount(6, countResult.value));
 
 const chance = computed(() => {
     if (dice.value.length == 0) {
-        return null;
+        return 0;
     }
 
     return dice.value.reduce((accumulator, currentValue) => accumulator + currentValue);
@@ -120,58 +120,58 @@ const yahtzee = computed(() => scoreXOfAKind(5, countResult.value, yahtzeeScore)
         <tbody>
             <tr>
                 <td>Enen</td>
-                <td>{{ aces ?? '-' }}</td>
+                <td>{{ aces }}</td>
             </tr>
             <tr>
                 <td>Tweeën</td>
-                <td>{{ twos ?? '-' }}</td>
+                <td>{{ twos }}</td>
             </tr>
             <tr>
                 <td>Drieën</td>
-                <td>{{ threes ?? '-' }}</td>
+                <td>{{ threes }}</td>
             </tr>
             <tr>
                 <td>Vieren</td>
-                <td>{{ fours ?? '-' }}</td>
+                <td>{{ fours }}</td>
             </tr>
             <tr>
                 <td>Vijven</td>
-                <td>{{ fives ?? '-' }}</td>
+                <td>{{ fives }}</td>
             </tr>
             <tr>
                 <td>Zessen</td>
-                <td>{{ sixes ?? '-' }}</td>
+                <td>{{ sixes }}</td>
             </tr>
             <tr>
                 <td colspan="2"></td>
             </tr>
             <tr>
                 <td>Chance</td>
-                <td>{{ chance ?? '-' }}</td>
+                <td>{{ chance }}</td>
             </tr>
             <tr>
                 <td>Three of a kind</td>
-                <td>{{ threeOfAKind ?? '-' }}</td>
+                <td>{{ threeOfAKind }}</td>
             </tr>
             <tr>
                 <td>Four of a kind</td>
-                <td>{{ fourOfAKind ?? '-' }}</td>
+                <td>{{ fourOfAKind }}</td>
             </tr>
             <tr>
                 <td>Full House</td>
-                <td>{{ fullHouse ?? '-' }}</td>
+                <td>{{ fullHouse }}</td>
             </tr>
             <tr>
                 <td>Small Straight</td>
-                <td>{{ smallStraight ?? '-' }}</td>
+                <td>{{ smallStraight }}</td>
             </tr>
             <tr>
                 <td>Large Straight</td>
-                <td>{{ largeStraight ?? '-' }}</td>
+                <td>{{ largeStraight }}</td>
             </tr>
             <tr>
                 <td>Yahtzee</td>
-                <td>{{ yahtzee ?? '-' }}</td>
+                <td>{{ yahtzee }}</td>
             </tr>
         </tbody>
     </table>
