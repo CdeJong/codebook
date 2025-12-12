192bf5fdd1180889e833d875feecb1375fd27391
CdeJong <christiaan240@gmail.com>
2025-12-12 11:52:52 +0100

    Tried implementing Typed parameters

--- vuejs/inventory-system/src/domain/product/routes.ts
+++ vuejs/inventory-system/src/domain/product/routes.ts
@@ -1,3 +1,4 @@
+//import { type RouteRecordInfo } from 'vue-router';
 import HomePage from '@/domain/product/page/HomePage.vue';
 import CreatePage from '@/domain/product/page/CreatePage.vue';
 import EditPage from '@/domain/product/page/EditPage.vue';
@@ -12,3 +13,53 @@ export const routes = [
     {path: '/overview', component: OverviewPage},
     {path: '/order', component: OrderPage},
 ];
+
+// export interface RouteNamedMap {
+
+//     home: RouteRecordInfo<
+//         'home',
+//         '/',
+//         Record<never, never>,
+//         Record<never, never>,
+//         never
+//     >
+
+//     create: RouteRecordInfo<
+//         'create',
+//         '/create',
+//         Record<never, never>,
+//         Record<never, never>,
+//         never
+//     >
+
+//     edit: RouteRecordInfo<
+//         'edit',
+//         '/edit/:id',
+//         { id: number }, // parsed to?
+//         { id: string }, // url value?
+//         never
+//     >
+
+//     overview: RouteRecordInfo<
+//         'overview',
+//         '/overview',
+//         Record<never, never>,
+//         Record<never, never>,
+//         never
+//     >
+
+//     order: RouteRecordInfo<
+//         'order',
+//         '/order',
+//         Record<never, never>,
+//         Record<never, never>,
+//         never
+//     >
+
+// }
+
+// declare module 'vue-router' {
+//   interface TypesConfig {
+//     RouteNamedMap: RouteNamedMap
+//   }
+// }
