7b033297ced874188e28dd4f1bf29b5cb60a9cb5
CdeJong <christiaan240@gmail.com>
2025-12-12 10:29:36 +0100

    Added a way to create/add new products

---
+++ vuejs/inventory-system/src/domain/product/component/ProductForm.vue
@@ -0,0 +1,81 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+import { type NewProduct } from '@/domain/product/model/product';
+
+const emit = defineEmits(["submit"]);
+
+interface Props {
+    product? : NewProduct,
+    clearOnSubmit: boolean,
+    submitButtonText: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+    product: () => ({name: '', price: 0, amount: 0, minimumAmount: 0}), 
+    clearOnSubmit: false, 
+    submitButtonText: 'Submit' 
+})
+const product : NewProduct = reactive({...props.product});
+
+const submit = () : void => {
+    emit("submit", {...product});
+    if (props.clearOnSubmit) {
+        product.name = '';
+        product.price = 0;
+        product.minimumAmount = 0;
+        product.amount = 0;
+    }
+}
+</script>
+
+<template>
+    <form @submit.prevent="submit()">
+        <label for="name">Name:</label>
+        <input id="name" type="text" placeholder="Bread" required v-model="product.name">
+
+        <label for="price">Price:</label>
+        <input id="price" type="text" placeholder="1.99" required v-model.number="product.price">
+
+        <label for="amount">Amount:</label>
+        <input id="amount" type="number" min="0" placeholder="0" v-model="product.amount">
+
+        <label for="minimum-amount">Minimum Amount:</label>
+        <input id="minimum-amount" type="number" min="0" placeholder="0" v-model="product.minimumAmount">
+
+        <button type="submit">{{ props.submitButtonText }}</button>
+    </form>
+</template>
+
+<style scoped>
+form {
+    display: flex;
+    flex-direction: column;
+    width: min(600px, 100%);
+}
+
+input {
+    border: none;
+    border-bottom: 1px solid black;
+}
+
+input {
+    margin-bottom: 10px;
+    /* width: 100%; */
+}
+
+input, label, button {
+    padding: 5px 10px;
+}
+
+button {
+    align-self: flex-start;
+    background-color: aqua;
+    border: none;
+    border-radius: 5px;
+    min-width: 100px;
+}
+
+button:hover {
+    color: white;
+}    
+</style>
---
+++ vuejs/inventory-system/src/domain/product/page/CreatePage.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ProductForm from '@/domain/product/component/ProductForm.vue';
+import { addProduct } from '@/domain/product/store.ts';
+import { useRouter, type Router } from 'vue-router';
+import { type NewProduct } from '@/domain/product/model/product';
+
+const router : Router = useRouter();
+
+const submit = (product : NewProduct) : void => {
+    addProduct(product);
+    router.push('/overview');
+}
+</script>
+
+<template>
+    <h1 class="title">Add a product to your shopping list!</h1>
+    <ProductForm @submit="submit" clearOnSubmit submitButtonText="Add" />
+</template>
+
+<style scoped></style>
