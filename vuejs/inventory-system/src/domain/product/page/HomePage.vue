500ba0cae660d1ac7910528c40409a3ee0d8c2ec
CdeJong <christiaan240@gmail.com>
2025-12-12 10:28:45 +0100

    Added the home page

---
+++ vuejs/inventory-system/src/domain/product/page/HomePage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+    console.log("test");
+</script>
+
+<template>
+    <h1 class="title">Home</h1>
+    <p>Nothing on the homepage...</p>
+</template>
+
+<style scoped>
+p {
+    padding: 0 5px;
+}
+</style>
