8601cebc6b385e9788371bca4b1e5b31a3e27980
CdeJong <christiaan240@gmail.com>
2025-12-29 10:50:00 +0100

    Added error messages for the create form

--- php/php-shopping-list/controller/create.php
+++ php/php-shopping-list/controller/create.php
@@ -3,9 +3,15 @@ require "Database.php";
 require "config.php";
 $database = new Database($databaseCredentials);
 
-if ($_SERVER["REQUEST_METHOD"] === 'GET') {
+if ($_SERVER["REQUEST_METHOD"] === "GET") {
+    $error = $_GET["error"] ?? null;
+    $hasEmptyFields = $error === "empty-fields";
+    $hasInvalidName = $error === "invalid-name";
+    $hasInvalidQuantity = $error === "invalid-quantity";
+    $hasInvalidPrice = $error === "invalid-price";
+
     require "view/create.view.php";
-} else if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+} else if ($_SERVER["REQUEST_METHOD"] === "POST") {
     handlePost($routes, $database);    
 } else {
     http_response_code(405);
@@ -23,7 +29,7 @@ function handlePost($routes, $database) {
     }
 
     if (!ValidatorUtil::string($name, 1, 50)) { // 50 is current database limit
-        redirect("product.create", $routes, ["error" => "invalid-name-" . mb_strlen($name, "utf-8")]);
+        redirect("product.create", $routes, ["error" => "invalid-name"]);
         return;
     }
 
--- php/php-shopping-list/view/create.view.php
+++ php/php-shopping-list/view/create.view.php
@@ -5,14 +5,23 @@ require "view/partial/header.php";
 <form action="/create" method="POST">
     <label for="name">Name:</label>
     <input type="text" placeholder="Bread" required name="name">
-
+    <?php if ($hasInvalidName): ?>
+    <p class="error">Invalid name! Name uses disallowed characters or is too long.</p>
+    <?php endif; ?>
     <label for="name">Price:</label>
     <input type="text" placeholder="1.99" required name="price">
-
-    <label for="name">Amount:</label>
+    <?php if ($hasInvalidPrice): ?>
+    <p class="error">Invalid price! The price should be a positive (decimal) number.</p>
+    <?php endif; ?>
+    <label for="name">Quantity:</label>
     <input type="number" min="0" placeholder="0" name="quantity">
-
+    <?php if ($hasInvalidQuantity): ?>
+    <p class="error">Invalid quantity! The quantity should be a positive (non-decimal) number.</p>
+    <?php endif; ?>    
     <button type="submit" name="submit">Create</button>
+    <?php if ($hasEmptyFields): ?>
+    <p class="error">One or more input fields were empty, be sure to fill out all required input fields!</p>
+    <?php endif; ?>
 </form>    
 <?php
 require "view/partial/footer.php";
