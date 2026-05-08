c56ff583ef5772a6fc97ea812ba38d8af1dc26f6
CdeJong <christiaan240@gmail.com>
2026-05-08 12:24:50 +0200

    Removed unused styling in TicketTable

--- laravel-vue/ticket-system/resources/js/domains/tickets/components/TicketTable.vue
+++ laravel-vue/ticket-system/resources/js/domains/tickets/components/TicketTable.vue
@@ -12,7 +12,6 @@ const tickets = defineModel<Ticket[]>();
             <tr>
                 <th class="id"></th>
                 <th>Title</th>
-                <!-- <th>Categories</th> -->
                 <th>Status</th>
                 <th>Created By</th>
                 <th>Assigned To</th>
@@ -32,40 +31,8 @@ const tickets = defineModel<Ticket[]>();
 </template>
 
 <style scoped>
-
 .table .id {
     min-width: 0;
-}
-
-.table .unassigned {
-    color: #888;
-}
-
-.categories {
-    display: flex;
-    flex-wrap: nowrap;
-    max-width: 125px;
-    overflow: hidden;
-    gap: 0.25em;
-    position: relative;
-}
-
-.category {
-    flex: 0 0 auto;
-    padding: 0.25em;
-    background-color: aqua;
-    border-radius: 10%;
-}
-
-.categories::after {
-  content: "";
-  position: absolute;
-  top: 0;
-  right: 0;
-  width: 30px; /* fade width */
-  height: 100%;
-  pointer-events: none;
-  background: linear-gradient(to right, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 100%);
 }
 
 .table tbody tr:hover .categories::after {
